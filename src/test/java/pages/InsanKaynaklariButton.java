e69df355658939cf95cd2f4dad6918c251db77cb
Hilal Dongel <hilaldongel@gmail.com>
2023-12-25 15:14:50 +0000

    V1

---
+++ src/test/java/pages/InsanKaynaklariButton.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class InsanKaynaklariButton {
+
+    public InsanKaynaklariButton(){
+
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "")
+    public WebElement lrektj;
+}
